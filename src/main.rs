a4a1377611081003c992fc2f56f7953562766ada
nevy11 <smongare2004@gmail.com>
2025-09-02 20:31:02 +0300

    provide a way to pass python code to a function to process it

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,19 @@
 use pyo3::prelude::*;
-use rust_codevia::python_interpreter::convert_case::run_python_code;
+use rust_codevia::python_interpreter::convert_case::{process_python_string, run_python_code};
 fn main() -> PyResult<()> {
     let result: String = run_python_code();
     println!("{}", result);
+
+    let code = r#"
+print("hello from rust")
+a = 5
+b = 10
+c = a + b
+print(c)
+result = c
+"#;
+
+    let output = process_python_string(code);
+    println!("Result from Python: {}", output);
     Ok(())
 }
--- src/python_interpreter/convert_case.rs
+++ src/python_interpreter/convert_case.rs
@@ -1,10 +1,61 @@
 use pyo3::ffi::c_str;
 use pyo3::prelude::*;
+use pyo3::types::IntoPyDict;
+use std::ffi::CString;
 
 pub fn run_python_code() -> String {
     Python::attach(|py| {
         let code = c_str!("'Hello from embedded Python!'");
         let result: String = py.eval(code, None, None).unwrap().extract().unwrap();
+        result
+    })
+}
+
+// pub fn process_python_string(input: &str) -> String {
+//     Python::attach(|py| {
+//         let code = c_str!("input_str");
+//         let locals = [("input_str", input)].into_py_dict(py);
+//         let result: String = py
+//             .eval(code, None, Some(&locals.unwrap()))
+//             .unwrap()
+//             .extract()
+//             .unwrap();
+//         result
+//     })
+// }
+
+pub fn process_python_string(code: &str) -> i32 {
+    Python::attach(|py| {
+        // Locals for variable sharing
+        let locals = [("input_str", "Hello from Rust!")]
+            .into_py_dict(py)
+            .unwrap();
+
+        // Multi-line Python code
+        //         let code = r#"
+        // print("hello from rust")
+        // a = 5
+        // b = 10
+        // c = a + b
+        // print(c)
+        // result = c  # store result so Rust can extract it
+        // "#;
+
+        // Convert Rust string -> CString for safety
+        let c_code = CString::new(code).unwrap();
+        let c_code_str = c_code.as_c_str();
+
+        // Run the Python block
+        py.run(c_code_str, None, Some(&locals)).unwrap();
+
+        // Extract the result into Rust
+        let result: i32 = locals
+            .get_item("result")
+            .unwrap()
+            .unwrap()
+            .extract()
+            .unwrap();
+
         result
     })
 }
